5058875520d879095e15edfc4e2a625b21bc8546
abbasmuradzada <abbasam@code.edu.az>
2020-07-12 04:06:46 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var squareCalculator = document.getElementById("square-input");
+var squareButton = document.getElementById("square-button");
+
+var halfNumberCalculator = document.getElementById("half-input");
+var halfButton = document.getElementById("half-button");
+
+var fraction = document.getElementById("percent1-input");
+var whole = document.getElementById("percent2-input");
+var floatFinder = document.getElementById("percent-button");
+
+var areaCalculator = document.getElementById("area-input");
+var areaButton = document.getElementById("area-button");
+
+var solution = document.getElementById("solution");
+
+squareButton.addEventListener("click",function(){
+    solution.innerText=`Square of ${squareCalculator.value} is ${(Math.pow(squareCalculator.value,2))}`;
+})
+
+halfButton.addEventListener("click",function(){
+    solution.innerText=`Half of ${halfNumberCalculator.value} is ${(halfNumberCalculator.value/2)}`;
+})
+
+floatFinder.addEventListener("click",()=>{
+    solution.innerText=(fraction.value/whole.value);
+})
+
+areaButton.addEventListener("click",function(){
+    solution.innerText=`Area of circle that has ${areaCalculator.value} radius is ${Math.round((Math.PI*Math.pow(areaCalculator.value,2)))} (exactly ${(Math.PI*Math.pow(areaCalculator.value,2))})`
+})
